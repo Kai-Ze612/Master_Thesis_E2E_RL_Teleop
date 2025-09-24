19d0a5bc525ffadc3dabcd858659a3c5e678d589
Kai-Ze,Deng <ge62meq@mytum.de>
2025-09-24 13:29:38 +0200

    add new project

--- libfranka_ws/src/Reinforcement_Learning_In_Teleoperation/setup.py
+++ libfranka_ws/src/Reinforcement_Learning_In_Teleoperation/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'Reinforcement_Learning_In_Teleoperation'
 
@@ -10,16 +12,25 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Add launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Optional: Add config files if you have them
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # Optional: Add additional resources
+        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='kai',
     maintainer_email='ge62meq@mytum.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Reinforcement Learning in Teleoperation',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'remote_node = Reinforcement_Learning_In_Teleoperation.nodes.follower_real_robot:main',
+            'local_node = Reinforcement_Learning_In_Teleoperation.nodes.leader_mujoco_simulator:main',
+            'remote_simulated_node = Reinforcement_Learning_In_Teleoperation.nodes.follower_mujoco_simulator:main'
         ],
     },
 )
